da98b753ac6cf638eb196560b03b1c6d0e9f1063
farhanhdyt <44648039+farhanhdyt@users.noreply.github.com>
2018-10-31 23:56:39 +0700

    Update QueueFloat.java

--- QueueFloat.java
+++ QueueFloat.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class QueueFloat {
 	ArrayList<Float> kelase = new ArrayList<Float>();
-	int alief = -1;
+	int farhan = -1;
 
 	public void insert(Float value) {
-		alief++;
-		kelase.add(alief,value);
+		farhan++;
+		kelase.add(farhan,value);
 	}
 	public Float get() {
 		Float value = 0.1f;
-		if(alief>-1) {
+		if(farhan>-1) {
 			value = kelase.get(0);
 			kelase.remove(0);
-			alief--;
+			farhan--;
 		}
 		return value;
 		
 	}
 	public void cetak() {
-		System.out.println("ECOR : "+alief+" > VARIABLE: "+kelase.toString());
+		System.out.println("ECOR : "+farhan+" > VARIABLE: "+kelase.toString());
 	}
 }
