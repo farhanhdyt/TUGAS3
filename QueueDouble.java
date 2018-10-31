53cc0a1ff3f661ff37b1949e2ce545d942df910d
farhanhdyt <44648039+farhanhdyt@users.noreply.github.com>
2018-10-31 23:58:28 +0700

    Update QueueDouble.java

--- QueueDouble.java
+++ QueueDouble.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class QueueDouble {
 	ArrayList<Double> kelase = new ArrayList<Double>();
-	int alief = -1;
+	int farhan = -1;
 
 	public void insert(Double value) {
 		alief++;
-		kelase.add(alief,value);
+		kelase.add(farhab,value);
 	}
 	public Double get() {
 		Double value = 0.1;
-		if(alief>-1) {
+		if(farhan>-1) {
 			value = kelase.get(0);
 			kelase.remove(0);
-			alief--;
+			farhan--;
 		}
 		return value;
 		
 	}
 	public void cetak() {
-		System.out.println("ECOR : "+alief+" > VARIABLE: "+kelase.toString());
+		System.out.println("ECOR : "+farhan+" > VARIABLE: "+kelase.toString());
 	}
 }
